6077847e706e63ed37e7493efbd43cedf23094b5
Bart Janssens <bart@bartjanssens.org>
2018-07-28 14:35:58 +0200

    Add appveyor file

--- structs.jl
+++ structs.jl
@@ -8,6 +8,6 @@ struct TwoWrappedInts
 end
 
 println("This should print \"1\" :")
-ccall((:print_one,"structs.so"), Cvoid, (OneWrappedInt,), OneWrappedInt(1))
+ccall((:print_one,"structs"), Cvoid, (OneWrappedInt,), OneWrappedInt(1))
 println("This should print \"1 2\" :")
-ccall((:print_two,"structs.so"), Cvoid, (TwoWrappedInts,), TwoWrappedInts(1,2))
+ccall((:print_two,"structs"), Cvoid, (TwoWrappedInts,), TwoWrappedInts(1,2))
